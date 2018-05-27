44ab878a045919d48609a4e166c6336d51291405
tataqwerty <tarasik2000@gmail.com>
2018-05-27 21:43:05 +0300

    гавнокод

--- lem-in.c
+++ lem-in.c
@@ -18,31 +18,33 @@ void		listdel(t_list **head)
 	*head = NULL;
 }
 
-void		ft_list_pushback(t_list **head, char *line)
+void		roomsdel(t_room *room)
 {
-	t_list	*tmp;
-	t_list	*new;
 
-	tmp = *head;
-	while (tmp)
-	{
-		if (tmp->next)
-			tmp = tmp->next;
-		else
-			break ;
-	}
-	if (!(new = (t_list*)malloc(sizeof(t_list))))
-		return ;
-	new->content = ft_strdup(line);
-	new->content_size = 0;
-	new->next = NULL;
-	if (!*head)
-		*head = new;
-	else
-		tmp->next = new;
 }
 
-void		get_all_lines(t_list **head, int *lines)
+//////////////////
+void		ft_puterror(char *str)
+{
+	ft_putstr("Error: ");
+	ft_putstr(str);
+}
+
+void		error_management(char error)
+{
+	if (error == INV_FILE)
+		ft_puterror("Invalid file.\n");
+	else if (error == INV_ANTS)
+		ft_puterror("Invalid quantity of ants.\n");
+	else if (error == LINK_BEFORE_ROOM)
+		ft_puterror("No room yet.\n");
+	else if (error == WTF)
+		ft_puterror("wtf?.\n");
+	
+}
+//////// Вместо этой хуйни макрос зафигачить.
+
+void		get_all_lines(t_list **head)
 {
 	char	*line;
 
@@ -51,116 +53,163 @@ void		get_all_lines(t_list **head, int *lines)
 	{
 		ft_list_pushback(head, line);
 		free(line);
-		*lines++;
 	}
 }
 
-char		get_rooms_counter(t_str *s, t_list **list)
+char		get_counter_ants(t_list **list)
 {
-	if (!*list || !(*list)->content)
-	{
-		ft_putstr("Empty file\n");
-		return (0);
-	}
-	s->rooms = ft_atoi((*list)->content);
-	if (s->rooms == 0)
-	{
-		ft_putstr("Quantity of rooms equal to zero!\n");
-		return (0);
-	}
+	int		i;
+
+	i = 0;
+	if (!list || !*list || (*list)->content_size == 0)
+		return (INV_FILE); // Invalid file.
+	while (((char*)(*list)->content)[i] >= '0' && ((char*)(*list)->content)[i] <= '9')
+		s->rooms_counter = s->rooms_counter * 10 + ((char*)(*list)->content)[i++] - 48;
+	if (s->rooms_counter == 0 || ((char*)(*list)->content)[i] != '\0')
+		return (INV_ANTS); // Invalid quantity of ants.
 	*list = (*list)->next;
-	return (1);
+	return (0);
+}
+
+char		is_command(char *str)
+{
+	if (ft_strcmp(str, "##start") == 0 || ft_strcmp(str, "##end") == 0)
+		return (1);
+	return (0);
 }
 
 char		is_room(char *str)
 {
-	char tmp;
-	int i;
+	char	flag;
+	int		i;
 
-	tmp = 0;
 	i = 0;
-	if (!str || str[0] == '\0' || str[0] == 'L' || str[0] == '#')
+	if (!str || str[0] == 'L' || str[i] == '#')
 		return (0);
-	while (str[i] > 32 && str[i] < 127 && i++)
-		tmp = 1;
-	if (tmp == 0 || str[i++] != ' ')
+	flag = 0;
+	while (str[i] != '-' && str[i] > 32 && str[i] < 127 && i++)
+		flag = 1;
+	if (flag == 0 || str[i++] != ' ')
 		return (0);
-	tmp = 0;
+	flag = 0;
 	while (str[i] >= '0' && str[i] <= '9' && i++)
-		tmp = 1;
-	if (tmp == 0 || str[i++] != ' ')
+		flag = 1;
+	if (flag == 0 || str[i++] != ' ')
 		return (0);
-	tmp = 0;
+	flag = 0;
 	while (str[i] >= '0' && str[i] <= '9' && i++)
-		tmp = 1;
-	if (tmp == 0 || str[i++] != '\0')
+		flag = 1;
+	if (flag == 0 || str[i] != '\0')
 		return (0);
 	return (1);
 }
 
 char		is_link(char *str)
 {
-	char	tmp;
 	int		i;
+	char	flag;
 
-	tmp = 0;
+	flag = 0;
 	i = 0;
 	if (!str)
 		return (0);
-	while (str[i] != '\0' && str[i] != '-'
-		&& str[i] > 32 && str[i] < 127 && i++)
-		tmp = 1;
-	if (tmp == 0 || str[i++] != '-')
+	while (str[i] != '-' && str[i] > 32 && str[i] < 127 && i++)
+		flag = 1;
+	if (flag == 0 || str[i++] != '-')
 		return (0);
-	tmp = 0;
-	while (str[i] != '\0' && str[i] != '-'
-		&& str[i] > 32 && str[i] < 127 && i++)
-		tmp = 1;
-	if (tmp == 0 || str[i] != '\0')
+	while (str[i] != '-' && str[i] > 32 && str[i] < 127 && i++)
+		flag = 1;
+	if (flag == 0 || str[i] != '\0')
 		return (0);
 	return (1);
 }
 
-char		add_room(t_str *s, char *line, char priority)
+char		add_room(char *str, int priority)
 {
-
+	
 }
 
-char		get_rooms(t_main *s, t_list **list)
+char		ft_for_command(t_list **list, char *flag)
 {
+	char	error;
+
+	if (ft_strcmp((*list)->content, "##start") == 0)
+	{
+		if ((*flag) & 1 == 1)
+			return (SEVERAL_STARTS);
+		*flag = (*flag) | 1;
+		if (!(*list = (*list)->next) || !is_room((*list)->content))
+			return (NOT_A_ROOM);
+		else if ((error = add_room((*list)->content, 1)) != 0)
+			return (error);
+	}
+	else if (ft_strcmp((*list)->content, "##end") == 0)
+	{
+		if ((*flag) & 2 == 2)
+			return (SEVERAL_ENDS);
+		*flag = (*flag) | 2;
+		if (!(*list = (*list)->next) || !is_room((*list)->content))
+			return (NOT_A_ROOM);
+		else if ((error = add_room((*list)->content, 2)) != 0)
+			return (error);
+	}
+	return (0);
+}
+
+char		get_rooms(t_str *s, t_list **list)
+{
+	char	flag;
+	char	error;
+
+	flag = 0;
 	while (*list)
 	{
-		if (ft_strcmp((*list)->content, "##start") || ft_strcmp((*list)->content, "##end"))
-		{
-			*list = (*list)->next;
-			if (is_room((*list)->content))
-			{
-				if (!add_room())
-					return (0);
-			}
+		if (is_command((*list)->content))
+			if ((error = ft_for_command(list, &flag)) != 0)
+				return (error);
+		else if (is_room((*list)->content))
+			if ((error = add_room((*list)->content, 0)) != 0)
+				return (error);
+		else if (is_link((*list)->content))
+			if (flag & 4 == 0) // линк перед комнатой.
+				return (LINK_BEFORE_ROOM);
 			else
-			{
-				ft_putstr("Error");
 				return (0);
-			}
-		}
+		else if ((*list)->content[0] != '#')
+			return (WTF); // ХЗ что за херня.
 		*list = (*list)->next;
 	}
-	return (1);
+	return (0);
+}
+
+char		get_links(t_str *s, t_list **list)
+{
+	// накостылить, чтоб брался первый линк.
+
 }
 
 char		parsing(t_str *s)
 {
-	t_list *list;
+	char	error;
+	t_list	*list;
 
 	s->line_list = NULL;
-	s->lines = 0;
-	get_all_lines(&s->line_list, &s->lines);
+	s->rooms_counter = 0;
+	s->room = NULL;
+	get_all_lines(&s->line_list);
 	list = s->line_list;
-	if (!get_rooms_counter(s, &list) || !get_rooms(s, &list) ||
-	!get_links(s, &list)) // Сдвигаю list и его не нужно фришить
+	if ((error = get_counter_ants(&list)) != 0)
 	{
-		listdel(s->line_list);
+		error_management(error);
+		listdel(&s->line_list);
+		return (0);
+	}
+	if ((error = get_rooms(s, &list)) != 0 || (error = get_links(s, &list)) != 0)
+	{
+		error_management(error);
+		listdel(&s->line_list);
+		if (s->room)
+			delrooms(&s->room);
 		return (0);
 	}
 	return (1);
@@ -173,6 +222,9 @@ int			main(void)
 	if (!(s = (t_str*)malloc(sizeof(t_str))))
 		return (0);
 	if (!parsing(s))
+	{
+		free(s);
 		return (0);
+	}
 	return (0);
 }
--- lem-in.h
+++ lem-in.h
@@ -1,5 +1,14 @@
 #ifndef LEM_IN_H
 # define LEM_IN_H
+
+# define INV_FILE 1
+# define INV_ANTS 2
+# define LINK_BEFORE_ROOM 3
+# define WTF 4
+# define SEVERAL_STARTS 5
+# define SEVERAL_ENDS 6
+# define NOT_A_ROOM 7
+
 typedef	struct		s_ant
 {
 	int				counter;
@@ -15,7 +24,7 @@ typedef struct		s_alist
 
 typedef struct		s_room
 {
-	char			starter_point : 2;
+	char			flags : 2;
 	char			*name;
 	t_alist			*neighbour;
 	struct s_room	*next;
@@ -24,8 +33,7 @@ typedef struct		s_room
 typedef struct
 {
 	int				ants;
-	int				rooms;
-	int				lines;
+	int				rooms_counter;
 	t_list			*line_list;
 	t_ant			*ant;
 	t_room			*room;
